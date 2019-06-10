2aeb79726eec80ff33be176d269205ecb92891da
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-06-10 14:38:43 +0545

    Creating Window

---
+++ Projectile Simulation/simulator.py
@@ -0,0 +1,22 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+window = 0                                             # glut window number
+width, height = 500, 400                               # window size
+
+def draw():                                            # ondraw is called all the time
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
+    glLoadIdentity()                                   # reset position
+    # ToDo draw rectangle
+    glutSwapBuffers()                                  # important for double buffering
+
+# initialization
+glutInit()                                             # initialize glut
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+glutInitWindowSize(width, height)                      # set window size
+glutInitWindowPosition(0, 0)                           # set window position
+window = glutCreateWindow("Projectile Simulator")              # create window with title
+glutDisplayFunc(draw)                                  # set draw function callback
+glutIdleFunc(draw)                                     # draw all the time
+glutMainLoop()                                         # start everything
