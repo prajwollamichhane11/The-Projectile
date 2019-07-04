9dfc09fe9970d0bf81b7fde316f68cefa11fce32
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-07-04 09:54:58 +0545

    Maximum Height

--- Projectile Simulation/test.py
+++ Projectile Simulation/test.py
@@ -1,37 +1,54 @@
-import turtle                                           # Import turtle library to use windows and graphical interface.
+import turtle
 import math
 
-PI = 3.14159265                                         # Setting PI as variable.
-V = int(input("Enter the Initial Velocity: "))                                                  # Setting first speed.
-aci = int(input("Enter the angle of inclination: "))                                                # Setting angle of throwing.
-g = 10                                                  # Setting gravitational acceleration.
+PI = 3.14159265
+g = 9.8
 
-Vx = V*math.cos(math.radians(aci))                      # Setting Vx speed
-Vy = V*math.sin(math.radians(aci))                      # Setting Vy speed
+V = int(input("Enter the Initial Velocity: "))                                                 
+aci = int(input("Enter the angle of inclination: "))
 
-Xo = 0                                          # Settin first X.
-Yo = 0                                          # Settin first Y.
+# Component of Initial Velocity on X-axis
+Vx = V*math.cos(math.radians(aci))
+# Component of Initial Velocity on Y-axis
+Vy = V*math.sin(math.radians(aci))
 
-turtle.setup(width=1250,height=600)             # Set sizes of window.
-turtle.setworldcoordinates(0, 0, 1250, 600)     # Set coordinatese where will object start from.
 
+# Initial point of the projectile
+# Point of Intersection of the axes
+Xo = 0
+Yo = 0
+
+# Setting the Window Sizes
+turtle.setup(width=1250,height=600)
+
+# Setting the Coordinates the object will start from and be displayed on the screen
+turtle.setworldcoordinates(0, 0, 1250, 600)
+
+# Finding the range for the first Projectile
 range = (V**2*math.sin(2*(math.radians(aci))))/9.8
 print("Range of First Projectile = " + str(range))
 
+# Finding the maximum height height of the first projectile
 maxheight = (V**2 * ((math.sin(math.radians(aci)))**2))/(2*9.8)
 print("Max Height of First Projectile = " + str(maxheight))
 
+
+#Drawing the Axes
+# X-axis
 turtle.setpos(0,0)
 turtle.left(0)
 turtle.pendown()
 turtle.forward(1250)
 turtle.penup()
 
+# Y-axis
 turtle.setpos(0,0)
 turtle.left(90)
 turtle.pendown()
 turtle.forward(600)
 turtle.penup()
+
+
 
 while Xo<1250:                      # X will be lower than window size.
     Tucus = (2 * Vy) / g                # Calculation of time of flying.
@@ -40,7 +57,6 @@ while Xo<1250:                      # X will be lower than window size.
         x = Xo + Vx*t                       # Calculation of "x" which is going to be used below to move turtle. ( circle ) 
         y = (Vy*t) - ((g*t**2)/2)           # Calculation of "y" which is going to be used below to move turtle. ( circle ) 
 
-        turtle.penup()
         turtle.shape("circle")              # Giving "circle" shape to the turtle object.
         turtle.shapesize(1)                 # Setting object size.
         turtle.pendown()
@@ -48,6 +64,16 @@ while Xo<1250:                      # X will be lower than window size.
 
         t=t+0.5                             # Increasing time (t) value.
 
+        if int(y) == int(maxheight):
+            xMaxheight = x
+            print(xMaxheight)
+            # Drawing Line from Maximun Height
+            turtle.setpos(xMaxheight,maxheight)
+            turtle.left(180)
+            turtle.pendown()
+            turtle.forward(maxheight)
+            turtle.penup()
+    
 
     Xo = Xo + Tucus*Vx                  # Setting new X.
     Vy = Vy*0.8                         # Setting new Y.
